4587fa4d78ac8f148efe595c91acc924f6809322
Henry Lilan <lilanhenry@gmail.com>
2012-11-05 00:23:53 +0300

    Added Investments Page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AffluenceMarketing::Application.config.secret_token = '643bdd3835e876eb91bcdb1d8755859eff62353c0180753cea58fe49bb10d9d31217814acb299e922c05c98e8cd360f45d4b292d372388c7ae92755182f762bc'
